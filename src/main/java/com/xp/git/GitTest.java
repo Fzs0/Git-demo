b45298014cb496bba0699bbd076f1e72ad53f8d8
whp <whp@qq.com>
2022-10-26 23:40:30 +0800

    first commit

---
+++ src/main/java/com/xp/git/GitTest.java
@@ -0,0 +1,14 @@
+package com.xp.git;
+
+/**
+ * @author xp
+ * @create 2022/10/26 22:52
+ * @Description:
+ */
+public class GitTest {
+
+    public static void main(String[] args) {
+        System.out.println("hello,git!");
+    }
+
+}
